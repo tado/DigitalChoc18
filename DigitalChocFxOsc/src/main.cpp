139ddcb91eb7c3a81c5e574290590a3dfa8c04e4
Atsushi Tadokoro <tadokoro@gmail.com>
2018-02-09 17:47:06 +0900

    udpate

---
+++ DigitalChocFxOsc/src/main.cpp
@@ -0,0 +1,8 @@
+#pragma comment(linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"")
+#include "ofMain.h"
+#include "ofApp.h"
+
+int main( ){
+    ofSetupOpenGL(1280, 720, OF_WINDOW);
+    ofRunApp(new ofApp());
+}
